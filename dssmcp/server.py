19948f0254f6736b747f56fa83093d0a05aecd84
Ned Martorell <32619850+nmartorell@users.noreply.github.com>
2026-01-29 16:17:21 -0500

    testing intial structure

---
+++ dssmcp/server.py
@@ -0,0 +1,6 @@
+import dataiku
+
+from fastmcp import FastMCP
+
+# Create MCP server
+mcp = FastMCP("Tools to interact with a Dataiku instance.")
---
+++ dssmcp/tools/dssclient.py
@@ -0,0 +1,13 @@
+import dataiku
+
+@mcp.tool
+def list_project_keys() -> list:
+    """
+    List the project keys (=project identifiers).
+
+    :returns: list of project keys identifiers, as strings
+    :rtype: list of strings
+    """
+    with dataiku.WebappImpersonationContext() as ctx:
+        client = dataiku.api_client()
+        return client.list_project_keys()
