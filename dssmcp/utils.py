3c8c001413a558d1f50da8cc64a8067a1b662ceb
Ned Martorell <32619850+nmartorell@users.noreply.github.com>
2026-02-04 16:38:21 -0500

    minor edits to utils.py

--- dssmcp/utils.py
+++ dssmcp/utils.py
@@ -1,13 +1,14 @@
-import dataikuapi
 from dataiku.core.intercom import get_backend_url
 from fastmcp.server.dependencies import get_http_request
+
+import dataikuapi
 
 
 def _get_impersonated_dss_client():
     auth_header = get_http_request().headers["authorization"]
-    dss_api_key = auth_header.split()[1]
+    dss_api_key = auth_header.split()[1]  # splits "Bearer <API_KEY>" string
     dss_backend_url = get_backend_url()
-    
+
     client = dataikuapi.DSSClient(dss_backend_url, dss_api_key)
-    client._session.verify = False # in case using self-signed certs for backend
+    client._session.verify = False  # in case using self-signed certs for backend
     return client
